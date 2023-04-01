326c10942dd7a10d0e118caa4c5c162afd0751f4
parsa <parsaloi35@gmail.com>
2023-04-01 17:58:52 +0300

    Parsa making the first commit to a git repo

---
+++ welcome-Parsa.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //this line will include information from the Standard library of the C programming language
+
+int main () //this line defines a function called main of type integer that receives no argument
+{
+    printf("Welcome to the G-gang\n"); //the main function calls a library function from the standard library called printf to print "Welcome to the G-gang"
+    return 0; //this line represents the end of the program where the main function exits the program and returns integer 0
+}
